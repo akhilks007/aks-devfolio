3196db004a83357d4197612008cd62a76f3199e9
Akhil <akhilks521@gmail.com>
2025-07-30 23:19:51 +0530

    [#3] - Header component for mobile devices

--- packages/devfolio-next-app/src/app/page.tsx
+++ packages/devfolio-next-app/src/app/page.tsx
@@ -1,12 +1,37 @@
-import { Wrapper } from "ui-components";
+// import { Wrapper } from 'ui-components';
+import Image from 'next/image';
+import classes from './styles.module.css';
 
 export default function Home() {
+  // return (
+  //   <Wrapper classNames='flex justify-center'>
+  //     <h1 className='bg-gradient-to-r from-amber-600 to-red-600 inline-block text-transparent bg-clip-text text-6xl'>
+  //       Devfolio
+  //     </h1>
+  //   </Wrapper>
+  // );
   return (
-    <Wrapper classNames='flex justify-center'>
-      <h1 className='bg-gradient-to-r from-amber-600 to-red-600 inline-block text-transparent bg-clip-text text-6xl'>
-        Devfolio
-      </h1>
-    </Wrapper>
+    <div className='main-container'>
+      <div
+        className={`header bg-indigo-950 shadow-header-shadow ${classes.header} h-[55px]`}>
+        <div className='wrapper flex p-[10px]'>
+          <div className='logo-container flex-1 pl-10'>
+            <div className='logo-wrapper relative w-8 h-8'>
+              <Image src='/logo.svg' alt='Logo' fill />
+            </div>
+          </div>
+          <div className='nav-container flex-1 pr-10 flex justify-center items-center'>
+            <div className='nav-wrapper'>
+              <ul className='flex gap-6'>
+                <li>Home</li>
+                <li>About</li>
+                <li>Lab</li>
+              </ul>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
 
