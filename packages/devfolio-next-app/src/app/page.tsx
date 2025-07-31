91140788ab594cac869ccb5b4b6491c3d5134457
Akhil <akhilks521@gmail.com>
2025-07-31 22:38:20 +0530

    [#3] - Favicon, transition, and responsive header

--- packages/devfolio-next-app/src/app/page.tsx
+++ packages/devfolio-next-app/src/app/page.tsx
@@ -15,14 +15,14 @@ export default function Home() {
       <div
         className={`header bg-indigo-950 shadow-header-shadow ${classes.header} h-[55px]`}>
         <div className='wrapper flex p-[10px]'>
-          <div className='logo-container flex-1 pl-10'>
-            <div className='logo-wrapper relative w-8 h-8'>
+          <div className='logo-container flex-1 flex justify-center'>
+            <div className={`logo-wrapper relative w-7 h-7 md:w-8 md:h-8`}>
               <Image src='/logo.svg' alt='Logo' fill />
             </div>
           </div>
           <div className='nav-container flex-1 pr-10 flex justify-center items-center'>
             <div className='nav-wrapper'>
-              <ul className='flex gap-6'>
+              <ul className='flex gap-6 text-[13px] md:text-[15px] md:gap-20'>
                 <li>Home</li>
                 <li>About</li>
                 <li>Lab</li>
